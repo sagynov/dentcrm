c76e9cc944025cb0180914b77d6162e9cc8d17ab
sagynov <kzxit99@gmail.com>
2025-03-22 21:54:32 +0500

    test fixed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'phone' => '777'.fake()->randomNumber(9, true),
+            'phone_verified_at' => now(),
+            'role' => 'owner',
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -11,7 +11,7 @@ test('registration screen can be rendered', function () {
 test('new users can register', function () {
     $response = $this->post('/register', [
         'name' => 'Test User',
-        'email' => 'test@example.com',
+        'phone' => '77757778899',
         'password' => 'password',
         'password_confirmation' => 'password',
     ]);
